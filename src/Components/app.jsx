a6f7262a8c11b3da37cb916d057d7fbcaef30c1c
Ayush Uprety <ayushuprety1@gmail.com>
2023-02-10 20:17:22 +0545

    Added dependencies and setup routing for the app

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React  from 'react';
+import { CssBaseline } from '@mui/material'
+import {Routes,Route} from 'react-router-dom'
+
 
 const App = () => {
   return (
     <div>
-      My app
+    <CssBaseline/>
+    <Routes>
+      <Route exact path='/' element={<Movies/>}/>
+      <Route exact path='/movies/:movieid' element={<MovieInformation/>}/>
+      <Route exact path='/actors/:actorid' element={<Actors/>}/>
+      <Route exact path='/profile/:userid' element={<Profile/>}/>
+    </Routes>
+      
     </div>
   )
 }
